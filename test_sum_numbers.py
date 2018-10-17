f8a856ddbd6b16a9966f3440ab1957ad5fd4b8f9
Sarah Putney <42679463+sputney13@users.noreply.github.com>
2018-10-17 00:16:31 -0400

    Add unit test for FileNotFoundError

--- test_sum_numbers.py
+++ test_sum_numbers.py
@@ -10,3 +10,7 @@ import pytest
 
 def test_sum_numbers(filename,expected):
     assert add_file_lines(filename) == expected
+
+def test_error():
+    with pytest.raises(FileNotFoundError):
+        add_file_lines("testfile4.txt")
